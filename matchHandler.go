314d8606db08a76b4f42c25d8affd71ef13721c5
Yadrishnikov Yaroslav <y.yadrushnikov@corp.vk.com>
2019-01-20 19:37:53 +0300

    fix

--- matchHandler.go
+++ matchHandler.go
@@ -1,5 +1,7 @@
 package main
 
+import "log"
+
 func handleMatch(whData webhookData) {
 	matchID := whData.MatchID
 	dbMatchData, _ := dao.get(dbMatch{MatchID: matchID})
@@ -10,14 +12,19 @@ func handleMatch(whData webhookData) {
 	if dbMatchData.IsFull != true {
 		matchData := getMatchData(matchID)
 		matchText := makeMatchText(matchData)
+		log.Println("match data is not full")
 		if whData.Origin == "scanner" && dbMatchData.IsShort != true {
+			log.Println("data from scanner and it's not posted yet")
 			makeMatchImage(matchData, false)
 			sendMatchToVk(matchID, matchText, false)
 		} else if whData.Origin == "" {
+			log.Println("data from parser")
 			makeMatchImage(matchData, true)
 			if dbMatchData.IsShort == true {
+				log.Println("data was posted, now editing")
 				editMatchAtVk(matchID, dbMatchData.PostID, matchText)
 			} else {
+				log.Println("data was never posted, now posting")
 				sendMatchToVk(matchID, matchText, true)
 			}
 		}
