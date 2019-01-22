2c76dacfab2c5b93a31ee3707b8e429ae686c38b
Yadrishnikov Yaroslav <y.yadrushnikov@corp.vk.com>
2019-01-22 19:44:45 +0300

    Prof

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/rs/cors"
 
@@ -22,6 +23,17 @@ func init() {
 }
 
 func main() {
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      http.DefaultServeMux,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+	}
+	go func() {
+		log.Println(http.ListenAndServe(":6060", nil))
+	}()
+	log.Fatal(server.ListenAndServe())
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", webhoookHandler)
 	srv := &http.Server{
