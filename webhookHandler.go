57351944df0bbe000242386fe814a69ee9b01875
Yaroslav Yadrishnikov <y.yadrushnikov@corp.vk.com>
2019-05-20 18:29:04 +0300

    Fix response

--- webhookHandler.go
+++ webhookHandler.go
@@ -10,15 +10,15 @@ func webhoookHandler(rw http.ResponseWriter, req *http.Request) {
 	body, err := ioutil.ReadAll(req.Body)
 	defer req.Body.Close()
 	if err != nil {
-		rw.Write([]byte(`{"status":fail}`))
+		rw.Write([]byte(`{"status":"fail"}`))
 		panic(err)
 	}
 	var whData oDotaMatchData
 	err = json.Unmarshal(body, &whData)
 	if err != nil {
-		rw.Write([]byte(`{"status":fail}`))
+		rw.Write([]byte(`{"status":"fail"}`))
 		panic(err)
 	}
-	rw.Write([]byte(`{"status":ok}`))
+	rw.Write([]byte(`{"status":"ok"}`))
 	handleMatch(whData)
 }
