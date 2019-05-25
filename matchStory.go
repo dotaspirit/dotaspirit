196b87ce3a06d1108119a13d73b717298f62d3fa
Yaroslav Yadrishnikov <y.yadrushnikov@corp.vk.com>
2019-05-25 17:52:04 +0300

    Fix nologo team

--- matchStory.go
+++ matchStory.go
@@ -103,7 +103,34 @@ func downloadTeamLogo(filepath, url string) (err error) {
 	return nil
 }
 
+func copyFile(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
+}
+
 func getTeamsLogo(radiantTeamID, direTeamID int) {
+	copyFile("assets/images/nologo.png", fmt.Sprintf("tmp/teams/%d.png", radiantTeamID))
+	copyFile("assets/images/nologo.png", fmt.Sprintf("tmp/teams/%d.png", direTeamID))
 	teamsData := getTeamsData()
 	for _, team := range teamsData {
 		teamID := team.TeamID
