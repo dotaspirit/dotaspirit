57b311b5297935fb7825a0ca4475a55cdd1a2ea1
Yaroslav Yadryshnikov <me@yay.qa>
2023-01-25 02:14:47 +0300

    TeaPot

--- cmd/dotaspirit/webhookHandler.go
+++ cmd/dotaspirit/webhookHandler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 )
 
@@ -11,18 +12,24 @@ func webhoookHandler(rw http.ResponseWriter, req *http.Request) {
 	defer req.Body.Close()
 	rw.Header().Add("Content-type", "application/json; charset=utf-8")
 	if err != nil {
+		rw.WriteHeader(http.StatusTeapot)
 		rw.Write([]byte(`{"status":"fail"}`))
-		panic(err)
+		log.Println(err)
+		return
 	}
-	if len(body) == 0 {
+	if req.Method != "POST" {
+		rw.WriteHeader(http.StatusTeapot)
 		rw.Write([]byte(`{"status":"fail"}`))
-		panic(err)
+		log.Println("Not post")
+		return
 	}
 	var whData oDotaMatchData
 	err = json.Unmarshal(body, &whData)
 	if err != nil {
+		rw.WriteHeader(http.StatusTeapot)
 		rw.Write([]byte(`{"status":"fail"}`))
-		panic(err)
+		log.Println(err)
+		return
 	}
 	rw.Write([]byte(`{"status":"ok"}`))
 	handleMatch(whData)
